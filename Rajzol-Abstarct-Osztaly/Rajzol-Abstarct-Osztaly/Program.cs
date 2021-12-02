defce2514cd6f52b0933fde8771634cbd0074e33
Pap <papd@kkszki.hu>
2021-12-02 12:35:52 +0100

    console project created

---
+++ Rajzol-Abstarct-Osztaly/Rajzol-Abstarct-Osztaly/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rajzol_Abstarct_Osztaly
+{
+    abstract class Grafobject
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
